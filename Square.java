2240e80dd2873ad19edd48cc0d161e6ff4339d1a
Minhhop09 <tranminhhop09@gmail.com>
2023-02-24 20:29:10 +0700

    exercise 1

---
+++ Square.java
@@ -0,0 +1,26 @@
+
+public class Square {
+    private double sideLength;
+
+    public Square(){}
+
+    public Square(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getArea() {
+        return sideLength * sideLength;
+    }
+
+    public double getPerimeter() {
+        return 4 * sideLength;
+    }
+}
